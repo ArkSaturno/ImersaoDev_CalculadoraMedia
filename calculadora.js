20778a47a00762765946fb4ba09b9e3da53acfd0
Victor <victorluzsd@gmail.com>
2022-09-06 22:32:10 -0300

    Alura_FirstProject
    My first project following the instructions from Alura, it has HTML, CSS and JavaScript

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+var nome = "Victor"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 8
+var notaDoTerceiroBimestre = 6
+var notaDoQuartoBimestre = 7
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4 .toFixed(1)
+
+console.log("Essa é sua nota " + nome)
+console.log(notaFinal)
