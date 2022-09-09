cb3322733efde95d88ec9126659e038f56e0d8bf
Victor <victorluzsd@gmail.com>
2022-09-08 22:05:10 -0300

    FinalCommit
    last modifications

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,50 @@
-var nome = "Victor"
+function calcularMedia() {
+  let nomeAluno = document.getElementById('nome').value 
+  let nota1 = document.getElementById('n1').value
+  let nota2 = document.getElementById('n2').value
+  let nota3 = document.getElementById('n3').value
+  let nota4 = document.getElementById('n4').value
+  let resultado = document.getElementById('resultado')
+  
+  if(nomeAluno === '') {
+    return alert('Digite seu nome')
+  } else if(nota1 === '') {
+    return alert('Digite sua nota')
+  }else if(nota2 === '') {
+    return alert('Digite sua nota')
+  }else if(nota3 === '') {
+    return alert('Digite sua nota')
+  }else if(nota4 === '') {
+    return alert('Digite sua nota')
+  }
 
-var notaDoPrimeiroBimestre = 9
-var notaDoSegundoBimestre = 8
-var notaDoTerceiroBimestre = 6
-var notaDoQuartoBimestre = 7
+  const soma = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4))
+  const media = soma / 4
+  const notaFixada = media.toFixed(1)
 
-var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4 .toFixed(1)
+  if(notaFixada >= 11) {
+    return resultado.innerHTML = `Nota Invalida ${notaFixada}`
+  }else if(notaFixada <= -1) {
+    return resultado.innerHTML = `Nota Invalida ${notaFixada}`
+  }
 
-console.log("Essa é sua nota " + nome)
-console.log(notaFinal)
+ if(notaFixada >= 7) {
+    return resultado.innerHTML = `
+    <h3>Parabéns ${nomeAluno}!</h3>
+    <p>Sua nota: ${notaFixada}</p>
+    <p>Você está aprovad(a)!</p>`
+  } 
+  if(notaFixada >= 5) {
+    return resultado.innerHTML = `
+    <h3>Olá ${nomeAluno}!</h3>
+    <p>Sua nota: ${notaFixada}</p>
+    <p>Se esforce na recuperação!</p>`
+  }else {
+    return resultado.innerHTML = `
+     <h3>Olá!</h3>
+    <p>Esta é a sua nota: ${notaFixada}</p>
+    <p>Você está reprovad(a) ${nomeAluno}.</p>
+    `
+  }
+}
+  
